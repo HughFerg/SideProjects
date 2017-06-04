5204c4e9c1d96f0cb15ca372bf0f01d381b146cb
hughferguson <hughferguson25@gmail.com>
2017-06-04 12:25:03 -0400

    Added updates

---
+++ Desktop/5er/5er/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  5er
+//
+//  Created by Hugh Ferguson on 6/3/17.
+//  Copyright © 2017 Hugh Ferguson. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
